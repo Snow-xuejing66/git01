0025e2e2ead84ec387efc7bf346cda5d9d085c4c
Bob <466985735@qq.com>
2020-09-10 21:48:44 +0800

    路由传递

---
+++ vue07/product/index.js
@@ -0,0 +1,7 @@
+// 默认导入
+import m from './m.js'
+// 按需导入
+import { x, y } from "./m.js"
+console.log(m.a)
+console.log(x)
+console.log(y)
---
+++ vue07/product/m.js
@@ -0,0 +1,10 @@
+const a = 20
+const b = 10
+// 默认导出
+export default {
+    a,
+    b
+}
+// 按需导出
+export const x = 999
+export const y = 740
